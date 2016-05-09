12517feaba8d121298ac2c8f2aed1772d218b475
Kevin Njeru <knjeru@gmail.com>
2016-05-09 12:46:54 -0600

    Updated cards table migration

--- migrations/20160509123437_cards.js
+++ migrations/20160509123437_cards.js
@@ -2,18 +2,23 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('cards', function(table) {
       table.increments('id');
-      table.integer('user');
+      table.integer('created_by');
       table.integer('deck');
       table.text('question').notNullable();
       table.text('answer').notNullable();
       table.boolean('correct_answer').defaultTo(false);
       table.boolean('incorrect_answer').defaultTo(false);
+      table.integer('answered_by');
       table
           .foreign('deck')
           .references('id')
           .inTable('decks');
       table
-          .foreign('user')
+          .foreign('created_by')
+          .references('id')
+          .inTable('users');
+      table
+          .foreign('answered_by')
           .references('id')
           .inTable('users');
   })
